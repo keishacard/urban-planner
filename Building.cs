2acfe18c6824963844c2059da9c1948ae2df0ef2
Keisha <49162665+keishacard@users.noreply.github.com>
2019-07-11 15:43:24 -0500

    initial commit

---
+++ Building.cs
@@ -0,0 +1,71 @@
+using System;
+
+namespace urban_planner
+{
+    public class Building
+    {
+        // private fields
+        private string _designer = "Keisha";
+
+        private DateTime _dateConstructed;
+
+        private string _address;
+
+        private string _owner;
+
+        // public properties
+
+        public int Stories { get; set; }
+
+        public double Width { get; set; }
+
+        public double Depth { get; set; }
+
+        public double Volume
+        {
+            get
+            {
+                return Width * Depth * (3 * Stories);
+            }
+        }
+
+        // constructor
+
+        public Building(string BuildingAddress)
+        {
+
+            _address = BuildingAddress;
+        }
+
+        // public methods
+
+        public void Construct()
+        {
+            _dateConstructed = DateTime.Now;
+        }
+
+        public void Purchase(string Purchaser)
+        {
+            _owner = Purchaser;
+        }
+
+        /* example output: 100 Main Street
+        // ---------------
+        // Designed by Steve Brownlee
+        // Constructed on 2/1/18 7:19:08 AM
+        // Owned by Bob Ross
+        // 16128 cubic meters of space*/
+
+        public void BuildinInfo()
+        {
+            Console.WriteLine($"{_address}");
+            Console.WriteLine("------------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic feet of space");
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace urban_planner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Building FiveOneTwoEigth = new Building("512 8th Avenue");
+
+            var Hogwarts = new Building("777 Forbidden Forest Way, Scottish Highlands, UK")
+            {
+                Stories = 17,
+                Depth = 500,
+                Width = 500
+
+            };
+
+            var DraculasCastle = new Building("Strada General Traian Moșoiu 24, Bran 507025, Romania")
+            {
+                Stories = 13,
+                Depth = 333,
+                Width = 333,
+            };
+
+            var WinchesterHouse = new Building("525 S Winchester Blvd, San Jose, CA 95128")
+            {
+                Stories = 7,
+                Depth = 54,
+                Width = 35,
+            };
+
+
+            Hogwarts.Construct();
+            Hogwarts.Purchase("Dumbledore");
+            Hogwarts.BuildinInfo();
+
+            DraculasCastle.Construct();
+            DraculasCastle.Purchase("Vlad the Impaler");
+            DraculasCastle.BuildinInfo();
+
+            WinchesterHouse.Construct();
+            WinchesterHouse.Purchase("Sarah Winchester");
+            WinchesterHouse.BuildinInfo();
+
+        }
+    }
+}
